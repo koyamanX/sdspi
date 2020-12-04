1e5292e06d1609ef5a9b378c9e34843e9be341b1
koyamanX <ckoyama.1996@gmail.com>
2020-12-05 04:28:45 +0900

    add CSRs

--- sdspi.h
+++ sdspi.h
@@ -18,12 +18,21 @@ declare sdspi {
 	output SCLK;
 
 	func_in reset();
-	output debug_out[8];
+	input read_adrs[10];
+	input write_adrs[10];
+	input byteen[3];
+	output rdata[32];
+	input wdata[32];
+	func_in read(read_adrs, byteen);
+	func_in write(write_adrs, byteen, wdata);
+	func_out valid();
+	func_out load_access_fault();
+	func_out store_amo_access_fault();
+	func_out interrupt_req;
+
 	output debug_status[8];
 	output debug_inited;
-	func_in read();
-	output data[4096];
-	func_out valid(data);
+	output debug_sign[16];
 }
 
 struct command_token_t {
@@ -40,6 +49,17 @@ struct response_1_t {
 	illegal_command[1];
 	erase_reset[1];
 	in_idle_state[1];
+};
+
+struct status_t {
+	//x[29];
+	error[1];
+	idle[1];
+	inited[1];
+};
+struct operation_t {
+	x[31];
+	read[1];
 };
 
 #define CMD0 	0x40_00_00_00_00_95
